90fd238cd2d9d8ca843a00e785ac1d6436fac8d1
Nurian Coelho <nuriancoelho@hotmail.com>
2022-01-25 08:52:21 -0300

    chore: create theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,28 @@
+export default {
+  colors: {
+    primary: '#142B5D',
+    secundary: '#244677',
+    textSecundary: '#757575',
+    textWhite: '#F1F1F1',
+    textBlack: '#424242',
+    textPlaceholder: '#495057',
+    red: '#FC3333',
+  },
+  fontFamily: {
+    montserratMedium: 'MontserratMedium',
+    montserratSemiBold: 'MontserratSemiBold',
+    montserratBold: 'MontserratBold',
+  },
+  fontSize: {
+    title: 20,
+    titleSecundary: 18,
+    text: 16,
+    TextSecundary: 14,
+  },
+  breakpoints: {
+    sm: 280,
+    md: 320,
+    lg: 375,
+    xl: 425,
+  },
+};
